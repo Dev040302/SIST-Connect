a137722649bddcca224018a55b93e885868790c0
push1970 <push1970@gmail.com>
2022-04-11 19:28:34 +0530

    uiUpdate

--- SIST-Conect/app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ SIST-Conect/app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -31,4 +32,5 @@ class LoginActivity : AppCompatActivity() {
 
         }
     }
+
 }
