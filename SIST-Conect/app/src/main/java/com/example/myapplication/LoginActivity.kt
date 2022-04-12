d7ce9aea44a9a65b39f40b9c654965c475bf1d19
push1970 <push1970@gmail.com>
2022-04-12 09:47:29 +0530

    Loginand Sigup UI update

--- SIST-Conect/app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ SIST-Conect/app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -3,13 +3,20 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.passwordtxt
+import kotlinx.android.synthetic.main.activity_login.registernotxt
+import kotlinx.android.synthetic.main.activity_login.signupbtn
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val text = "<font color=#FF000000>Don't have a account? </font><font color=#ffffff>Signup Below</font>"
+        signupnote.setText(Html.fromHtml(text))
 
         signinbtn.setOnClickListener {
 
